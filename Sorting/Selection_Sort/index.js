f767d0524a6b22f6af7763d8bfbd397427fc4d39
iredith <iredith0112@gmail.com>
2023-09-14 22:29:24 +0530

    Selection sort in js and py

---
+++ Sorting/Selection_Sort/index.js
@@ -0,0 +1,25 @@
+let initial_array = [13, 46, 24, 52, 20, 9];
+
+console.log("Before Sorting")
+console.log(initial_array);
+
+function Selection_sort(arr) {
+  const arr_length = arr.length;
+
+  for (let i = 0; i < arr_length; i++) {
+    min_value = i;
+    for (let j = i + 1; j < arr_length; j++) {
+      if (arr[j] < arr[min_value]) {
+        min_value = j;
+      }
+    }
+    let temp = arr[i];
+    arr[i] = arr[min_value];
+    arr[min_value] = temp;
+  }
+
+  console.log("After Sorting");
+  console.log(arr);
+}
+
+Selection_sort(initial_array);
---
+++ Sorting/Selection_Sort/index.py
@@ -0,0 +1,20 @@
+def Selection_sort():
+    initial_values = [13, 46, 24, 52, 20, 9]
+    print("Before Sorting")
+    for a in initial_values:
+        print(a, end=" ")
+    print()
+    
+    for i in range(len(initial_values)):
+        mini = i
+        for j in range(i+1, len(initial_values)):
+            if initial_values[j] < initial_values[mini]:
+                mini = j
+        initial_values[i], initial_values[mini] = initial_values[mini], initial_values[i]
+
+    print("After Sorting")
+    for a in initial_values:
+        print(a, end=" ")
+
+if __name__ == "__main__":
+    Selection_sort()
